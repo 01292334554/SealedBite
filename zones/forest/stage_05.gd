1c2f4a1f78ad8585b1c7771e43b80073314e22d6
securas <ruben.so.luis@gmail.com>
2020-01-02 16:51:14 +0900

    y sorting on forest stages

--- zones/forest/stage_05.gd
+++ zones/forest/stage_05.gd
@@ -5,8 +5,8 @@ func _ready() -> void:
 		"_on_entering_starting_area" )
 	_ret = $finish_position.connect( "body_entered", self, \
 		"_on_entering_finish_area" )
-	_ret = $finish_position2.connect( "body_entered", self, \
-		"_on_entering_finish_area2" )
+#	_ret = $finish_position2.connect( "body_entered", self, \
+#		"_on_entering_finish_area2" )
 
 
 func _on_entering_starting_area( _body ) -> void:
@@ -21,8 +21,8 @@ func _on_entering_finish_area( _body ) -> void:
 	gamestate.state.current_dir = 1
 	game.main.load_gamestate()
 
-func _on_entering_finish_area2( _body ) -> void:
-	gamestate.state.current_lvl = "res://zones/forest/stage_06.tscn"
-	gamestate.state.current_pos = "starting_position"
-	gamestate.state.current_dir = 1
-	game.main.load_gamestate()
+#func _on_entering_finish_area2( _body ) -> void:
+#	gamestate.state.current_lvl = "res://zones/forest/stage_06.tscn"
+#	gamestate.state.current_pos = "starting_position"
+#	gamestate.state.current_dir = 1
+#	game.main.load_gamestate()
